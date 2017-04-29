af0f039d4d184f921137d3c9dddb84eb9e4dfa8b
Sebastian Heimlén <sebastian@heimlen.se>
2017-04-29 18:28:45 +0200

    Strukturerade upp repot, la till Clion projekt för Uppgift3, lade till todo's i projektet på saker som måste fixas

---
+++ Uppgift3/square.cpp
@@ -0,0 +1,84 @@
+#include <gecode/driver.hh>
+#include <gecode/int.hh>
+#include <gecode/minimodel.hh>
+
+using namespace Gecode;
+
+class Square : public Script {
+protected:
+    //the size of the surrounding square
+    IntVar sizeOfSquare;
+    //the x-coordinates for the packed squares
+    IntVarArray xCoords;
+    //the y-coordinates for the packed squares
+    IntVarArray yCoords;
+public:
+    //static member function size that we may use
+    static int size() {
+        return 1;
+    }
+    //number of squares
+    const int n = 30;
+
+    //Constructor
+    Square(const SizeOptions& opt)
+            : Script(opt),
+         sizeOfSquare(*this, std::floor(std::sqrt((n*(n-1))*(2*n+1)/6)), (n*(n-1)/2)),
+         xCoords(*this, n ,0, (n*(n-1)/2) - 1),
+         yCoords(*this, n ,0, (n*(n-1)/2) - 1) {
+
+        //number of squares
+        const int n = opt.size();
+
+        //Constraint for position of
+        for (int i = 0; i < n; i++) {
+            rel(*this, xCoords[i], IRT_LQ, (n*(n-1)/2) - (n-i));
+            rel(*this, yCoords[i], IRT_LQ, (n*(n-1)/2) - (n-i));
+        }
+//TODO fix reificated propagators for coordinates and size described in #2 in instructions
+        //Constraints for x coordinates
+        for (int cx=0; cx<n; cx++) {
+            BoolVarArgs bx(*this,n,0,1);
+            for (int i=0; i<n; i++)
+                //xCoords[i] must be less than cx-(n-i)+1 and bigger than cx
+                dom(*this, xCoords[i], cx-(n-i)+1, cx, bx[i]);
+            linear(*this, saz, bx, IRT_EQ, w);
+        }
+
+//TODO fix the column and row reified propagators described in #3 in instructions
+        // Constraints for squares
+
+        branch(*this, xCoords, INT_VAR_MIN_MIN(), INT_VAL_MIN());
+        branch(*this, yCoords, INT_VAR_MIN_MIN(), INT_VAL_MIN());
+
+        /// Constructor
+
+//TODO figure out why the compiler is whining about this, it should be correct.
+    // Copy constructor
+    Square(bool share, Square& s) : Script(share, s) {
+        xCoords.update(*this, share, s.xCoords);
+        yCoords.update(*this, share, s.yCoords);
+        }
+
+    // Copy method
+    virtual Space* copy(bool share) {
+        return new Square(share, *this);
+    }
+
+    /// Print solution
+
+
+int main(int argc, char* argv[]) {
+    SizeOptions opt("Square");
+    opt.size(0);
+    opt.ipl(IPL_DOM);
+    opt.solutions(0);
+    opt.parse(argc,argv);
+    if (opt.size() < 1) {
+        std::cerr << "Error: number of squares to place must be at least 1!"
+                  << std::endl;
+        return 1;
+    }
+
+    Script::run<Square,DFS,SizeOptions>(opt);
+}
