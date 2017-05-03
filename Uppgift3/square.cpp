25ac52a2c15fa487531aa1a322932930f5aae9da
Sebastian Heimlén <sebastian@heimlen.se>
2017-05-03 11:39:23 +0200

    Ändrade constraint för non-overlapping squares, den borde vara korrekt nu

--- Uppgift3/square.cpp
+++ Uppgift3/square.cpp
@@ -13,41 +13,49 @@ protected:
     //the y-coordinates for the packed squares
     IntVarArray yCoords;
 public:
-    //static member function size that we may use
-    static int size() {
-        return 1;
-    }
-    //number of squares
+
+    //Hardcoded number of squares
     const int n = 30;
 
+    int size(int i) {
+        return n-i;
+    }
     //Constructor
     Square(const SizeOptions& opt)
             : Script(opt),
-         sizeOfSquare(*this, std::floor(std::sqrt((n*(n-1))*(2*n+1)/6)), (n*(n-1)/2)),
-         xCoords(*this, n ,0, (n*(n-1)/2) - 1),
-         yCoords(*this, n ,0, (n*(n-1)/2) - 1) {
+         sizeOfSquare(*this, std::floor(std::sqrt((n*(n-1))*(2*n+1)/6)), (n*(n+1)/2)),
+         xCoords(*this, n ,0, sizeOfSquare.max() - 1),
+         yCoords(*this, n ,0, sizeOfSquare.max() - 1) {
 
-        //number of squares
-        const int n = opt.size();
-
-        //Constraint for position of
+        //Constraint for "lower-right corner" to make sure the squares fit.
         for (int i = 0; i < n; i++) {
-            rel(*this, xCoords[i], IRT_LQ, (n*(n-1)/2) - (n-i));
-            rel(*this, yCoords[i], IRT_LQ, (n*(n-1)/2) - (n-i));
+            rel(*this, xCoords[i], IRT_LQ, sizeOfSquare.max() - (n-i));
+            rel(*this, yCoords[i], IRT_LQ, sizeOfSquare.max() - (n-i));
         }
-//TODO fix reificated propagators for coordinates and size described in #2 in instructions
-        //Constraints for x coordinates
-        for (int cx=0; cx<n; cx++) {
-            BoolVarArgs bx(*this,n,0,1);
-            for (int i=0; i<n; i++)
-                //xCoords[i] must be less than cx-(n-i)+1 and bigger than cx
-                dom(*this, xCoords[i], cx-(n-i)+1, cx, bx[i]);
-            linear(*this, saz, bx, IRT_EQ, w);
+//TODO control that these constraints are correct
+        //Constraint for non-overlapping squares.
+        for (int square = 0; square < n; square++) {
+            for (int otherSquare = square+1; otherSquare <= n; otherSquare++) {
+                BoolVar b(*this, 0, 1);
+                rel(*this, (xCoords[square] + (n-square) <= xCoords[otherSquare]) == b);
+                rel(*this, (xCoords[otherSquare] + (n-otherSquare) <= xCoords[square] == b));
+                rel(*this, (yCoords[square] + (n-square) <= yCoords[otherSquare]) == b);
+                rel(*this, (yCoords[otherSquare] + (n-otherSquare) <= yCoords[square] == b));
+            }
         }
+
 
 //TODO fix the column and row reified propagators described in #3 in instructions
-        // Constraints for squares
+        //Constraint for columns of x coordinates
+        for (int cx = 0; cx < sizeOfSquare.max(); cx++) {
+            BoolVarArgs bx(*this,sizeOfSquare.max(),0,1);
+            for (int i=0; i<sizeOfSquare.max(); i++)
+                //xCoords[i] must be less than cx-(n-i)+1 and bigger than cx
+                dom(*this, xCoords[i], cx-(n-i)+1, cx, bx[i]);
+            linear(*this, n, bx, IRT_EQ, sizeOfSquare);
+        }
 
+//TODO write a good branching heuristic
         branch(*this, xCoords, INT_VAR_MIN_MIN(), INT_VAL_MIN());
         branch(*this, yCoords, INT_VAR_MIN_MIN(), INT_VAL_MIN());
 
