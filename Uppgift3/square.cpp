4019b3b095293296a75615e3a45dee07c4fc3c14
Sebastian Heimlén <sebastian@heimlen.se>
2017-05-11 13:09:48 +0200

    Added some comments and copied no-overlap code to no-overlap.cpp as well, so that we do not trash that file.

--- Uppgift3/square.cpp
+++ Uppgift3/square.cpp
@@ -4,6 +4,7 @@
 using namespace Gecode;
 using namespace Gecode::Int;
 
+//Number of squares to place
 const int N = 6;
 
 void no_overlap(Home home,
@@ -217,12 +218,42 @@ int main(int argc, char* argv[]) {
     opt.parse(argc,argv);
     opt.propagation(Square::PROP_SPECIAL_NO_OVERLAP_PROPAGATOR, "special",
                     "special no-overlap-propagator");
+    //Comment out the line below to use Gecode's propagator for nooverlap.
     opt.propagation(Square::PROP_SPECIAL_NO_OVERLAP_PROPAGATOR);
 
     Script::run<Square, DFS, SizeOptions>(opt);
 }
 
 // The no-overlap propagator
+
+/*
+ *  Main author:
+ *     Christian Schulte <cschulte@kth.se>
+ *
+ *  Copyright:
+ *     Christian Schulte, 2009
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 class NoOverlap : public Propagator {
 protected:
     // The x-coordinates
